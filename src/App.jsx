064991fbae14970beac8398e18fecf530ca011b1
Prabhupratik Pattanaik <prabhupratikpattanaik@gmail.com>
2024-10-09 19:45:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from './Components/LoginForm/LoginForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LoginForm/>
+        
+    </>
+  )
+}
+
+export default App
